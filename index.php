d79eae31ba92540995ff9c0eeb8a009f95dd65c5
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-12-17 15:02:44 +0100

    finito il primo es di php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Helloworld</title>
+</head>
+<body>
+    <?php
+        $ciao = "ciao! questo è il mio primo esperimento con php!";
+        $count = str_word_count($ciao)
+    ?>
+    <p><?php echo $ciao ?></p>
+    <p><?php  echo $count + 1 ?></p>
+</body>
+</html>
